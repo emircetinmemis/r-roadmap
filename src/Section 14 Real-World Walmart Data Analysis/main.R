edb6f2f9c86eeb3e629842cd15304c4ff6d515c2
Emir Cetin Memis <emircetinmemis@gmail.com>
2024-02-20 22:13:20 +0300

    preparation for walmart data practice

---
+++ src/Section 14 Real-World Walmart Data Analysis/main.R
@@ -0,0 +1,6 @@
+# This needs to be done, I have settled this course notes as a project in src folders. If any path problem occurs, comment out or in this line.
+setwd("src/Section 14 Real-World Walmart Data Analysis")
+
+walmart_data <- read.csv("data/walmart_data.csv")
+walmart_features <- read.csv("data/walmart_features.csv")
+
